f0ea2cf32ce2dfe7c2fcdfce032f025e56b06b5b
ChoiDongHyeon <vwwwwvww@gmail.com>
2023-03-04 17:49:51 +0900

    commit

--- Assets/Script/TestCollistion.cs
+++ Assets/Script/TestCollistion.cs
@@ -23,26 +23,61 @@ public class TestCollistion : MonoBehaviour
 
     void Start()
     {
-        
+
     }
 
-   
+
     void Update()
     {
+        //local 좌표 -> world 좌표 -> Screan(화면)
+        //게임 세상의 좌표가 world좌표  그안에있는 개체들의 좌표를local좌표라고 합니다
 
+        //  Debug.Log(Input.mousePosition); //Screen 좌표  표시
+        //스크린 좌표를 알았으니 월드좌표로 변환하는 방법이 필요합니다
 
-        Vector3 look = transform.TransformDirection(Vector3.forward);
-
-        Debug.DrawRay(transform.position+Vector3.up, look*40, Color.red);  //시각적은로 보이게 테스트하기위한 레이저발사
-
-       RaycastHit[] hits;
-
-      hits =  Physics.RaycastAll(transform.position + Vector3.up, look, 40);
-
-        foreach(RaycastHit hit in hits)
+        //클릭한 지점에 월드좌표 구하기!  ScreenToWorldPoint -> 스크린에서 월드포인트로 옮겨주는 역활
+        if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log($"레이케스트{hit.collider.gameObject.name}");
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            Debug.DrawRay(Camera.main.transform.position, ray.direction * 100f, Color.red,2.0f);
+
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit, 100f))
+            {
+                Debug.Log($"명중{hit.collider.gameObject.name}");
+            }
         }
+
+    }
+}
+
+
+
+      // Vector3 mousePos= Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+
+
+    
+
+
+
+
+
+
+
+      //  Vector3 look = transform.TransformDirection(Vector3.forward);
+
+      //  Debug.DrawRay(transform.position+Vector3.up, look*40, Color.red);  //시각적은로 보이게 테스트하기위한 레이저발사
+
+      // RaycastHit[] hits;
+
+      //hits =  Physics.RaycastAll(transform.position + Vector3.up, look, 40);
+
+      //  foreach(RaycastHit hit in hits)
+      //  {
+      //      Debug.Log($"레이케스트{hit.collider.gameObject.name}");
+      //  }
 
 
         //RaycastHit[] hit;
@@ -50,5 +85,4 @@ public class TestCollistion : MonoBehaviour
         //{
         //    Debug.Log($"레이케스트{hit.collider.gameObject.name}");
         //}
-    }
-}
+ 
