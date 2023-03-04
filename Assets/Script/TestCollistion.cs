ed2ea91e8ff8c6d072e5ec69660d7a38725fd1f7
ChoiDongHyeon <vwwwwvww@gmail.com>
2023-03-04 17:12:10 +0900

    commit

--- Assets/Script/TestCollistion.cs
+++ Assets/Script/TestCollistion.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class TestCollistion : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("콜리젼");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("트리거");
+
+    }
+
+
+
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
-        
+
+
+        Vector3 look = transform.TransformDirection(Vector3.forward);
+
+        Debug.DrawRay(transform.position+Vector3.up, look*40, Color.red);  //시각적은로 보이게 테스트하기위한 레이저발사
+
+       RaycastHit[] hits;
+
+      hits =  Physics.RaycastAll(transform.position + Vector3.up, look, 40);
+
+        foreach(RaycastHit hit in hits)
+        {
+            Debug.Log($"레이케스트{hit.collider.gameObject.name}");
+        }
+
+
+        //RaycastHit[] hit;
+        //if ( Physics.Raycast(transform.position+Vector3.up, look,out hit,40))
+        //{
+        //    Debug.Log($"레이케스트{hit.collider.gameObject.name}");
+        //}
     }
 }
