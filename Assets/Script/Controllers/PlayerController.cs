48ab77f86e2aec4de54b52bfb142dec14d941017
ChoiDongHyeon <vwwwwvww@gmail.com>
2023-03-04 23:11:49 +0900

    commit

--- Assets/Script/Controllers/PlayerController.cs
+++ Assets/Script/Controllers/PlayerController.cs
@@ -5,135 +5,154 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float speed = 5.0f;
-
-    bool moveToDest = false;
     Vector3 destPos;
+
+    public enum PlayerState
+    {
+        Die,
+        Moving,
+        Idle,
+      
+    }
+    PlayerState state = PlayerState.Idle;
+
+
+
+    void UpdateDie()
+    {
+
+    }
+    void UpdateMoving()
+    {
+        Vector3 dir = destPos - transform.position; //방향을 찾는다
+        if (dir.magnitude < 0.001f)
+        {
+            state = PlayerState.Idle;
+        }
+        else
+        {
+            float moveDist = Mathf.Clamp(speed * Time.deltaTime, 0, dir.magnitude);
+            transform.position += dir.normalized * moveDist;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 0.5f * Time.deltaTime);
+            transform.LookAt(destPos);//쳐다보는 방향
+        }
+        wait_run_ratio = Mathf.Lerp(wait_run_ratio, 1, 10f * Time.deltaTime);
+        Animator anim = GetComponent<Animator>();
+        anim.SetFloat("wait_run_ratio", wait_run_ratio);
+        anim.Play("WAIT_RUN");
+    }
+
+
+
+
+    void UpdateIdle()
+    {
+        wait_run_ratio = Mathf.Lerp(wait_run_ratio, 0, 10f * Time.deltaTime);
+        Animator anim = GetComponent<Animator>();
+        anim.SetFloat("wait_run_ratio", wait_run_ratio);
+        anim.Play("WAIT_RUN");
+    }
+
 
     float wait_run_ratio = 0;
     void Update()
     {
-
-        if (moveToDest)
+        switch (state)
         {
-            Vector3 dir = destPos - transform.position; //방향을 찾는다
-            if (dir.magnitude < 0.001f)
-            {
-                moveToDest = false;
-            }
-            else
-            {
-                float moveDist = Mathf.Clamp(speed * Time.deltaTime, 0, dir.magnitude);
-                transform.position += dir.normalized * moveDist;
+            case PlayerState.Die:
+                UpdateDie();
+                break;
 
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 0.5f * Time.deltaTime);
-                transform.LookAt(destPos);//쳐다보는 방향
-            }
+            case PlayerState.Moving:
+                UpdateMoving();
+                break;
+
+            case PlayerState.Idle:
+                UpdateIdle();
+                break;
         }
 
-
-
-      
-        if (moveToDest)
-        {
-            wait_run_ratio = Mathf.Lerp(wait_run_ratio, 1, 10f * Time.deltaTime);
-            Animator anim = GetComponent<Animator>();
-            anim.SetFloat("wait_run_ratio", wait_run_ratio);
-            anim.Play("WAIT_RUN");
-
-        }
-        else
-        {
-            wait_run_ratio = Mathf.Lerp(wait_run_ratio, 0, 10f * Time.deltaTime);
-            Animator anim = GetComponent<Animator>();
-            anim.SetFloat("wait_run_ratio", wait_run_ratio);
-            anim.Play("WAIT_RUN");
-
-
-        }
     }
 
-
-        //transform.rotation = Quaternion.Euler(new Vector3(0.0f, Angle, 0.0f));
-
-        // transform.rotation = Quaternion.Euler(new Vector3(0.0f, Angle, 0.0f));
-
-        // transform.Rotate(new Vector3(0.0f,Angle, 0.0f));        
-        //transform.TransformDirection => Local좌표->World좌표로 변환 
-        //InverseTransformDirection => World좌표->Local좌표로 변환
-
-
-
-
-
     void Start()
-    {
-
-        Managers.Input.KeyAction -= OnKeyboard;
-        Managers.Input.KeyAction += OnKeyboard;
+    {  
         Managers.Input.mouseAction -= OnMouseClicked;
         Managers.Input.mouseAction += OnMouseClicked;
     }
 
     void OnMouseClicked(Define.MouseEvent Evt)
-    {
-
-
-        //if (Evt != Define.MouseEvent.Click)
-        //{
-        //    return;
-        //}
-        //Debug.Log("마우스클릭");
-
-
-
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        Debug.DrawRay(Camera.main.transform.position, ray.direction * 100f, Color.red, 2.0f);
-
-
-        //ayerMask mask = LayerMask.GetMask("Monster") | LayerMask.GetMask("Wall"); //레이어 사용
-
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 100f, LayerMask.GetMask("Wall")))
+    { 
+        if (state == PlayerState.Die)
         {
-            destPos = hit.point;
-            moveToDest = true;
-
-            //Debug.Log($"명중{hit.collider.gameObject.name}");
+            return;
         }
-
-    }
-
-    void OnKeyboard()
-    {
-        if (Input.GetKey(KeyCode.W))
+        else
         {
 
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            transform.position += (Vector3.forward.normalized * Time.deltaTime * speed);
+            Debug.DrawRay(Camera.main.transform.position, ray.direction * 100f, Color.red, 2.0f);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100f, LayerMask.GetMask("Wall")))
+            {
+                destPos = hit.point;
+                state = PlayerState.Moving;
+
+                
+            }
         }
-        if (Input.GetKey(KeyCode.S))
-        {
 
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
-
-            transform.position += (Vector3.back.normalized * Time.deltaTime * speed);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
-
-            transform.position += (Vector3.left.normalized * Time.deltaTime * speed);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
-
-            transform.position += (Vector3.right.normalized * Time.deltaTime * speed);
-        }
-        moveToDest = false;
     }
 }
+
+
+//    void OnKeyboard()
+//    {
+//        if (Input.GetKey(KeyCode.W))
+//        {
+
+//            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
+
+//            transform.position += (Vector3.forward.normalized * Time.deltaTime * speed);
+//        }
+//        if (Input.GetKey(KeyCode.S))
+//        {
+
+//            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
+
+//            transform.position += (Vector3.back.normalized * Time.deltaTime * speed);
+//        }
+//        if (Input.GetKey(KeyCode.A))
+//        {
+
+//            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
+
+//            transform.position += (Vector3.left.normalized * Time.deltaTime * speed);
+//        }
+//        if (Input.GetKey(KeyCode.D))
+//        {
+
+//            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
+
+//            transform.position += (Vector3.right.normalized * Time.deltaTime * speed);
+//        }
+//        moveToDest = false;
+//    }
+//}
+
+//transform.rotation = Quaternion.Euler(new Vector3(0.0f, Angle, 0.0f));
+
+// transform.rotation = Quaternion.Euler(new Vector3(0.0f, Angle, 0.0f));
+
+// transform.Rotate(new Vector3(0.0f,Angle, 0.0f));        
+//transform.TransformDirection => Local좌표->World좌표로 변환 
+//InverseTransformDirection => World좌표->Local좌표로 변환
+//if (Evt != Define.MouseEvent.Click)
+//{
+//    return;
+//}
+//Debug.Log("마우스클릭");
+//ayerMask mask = LayerMask.GetMask("Monster") | LayerMask.GetMask("Wall"); //레이어 사용
+//  Managers.Input.KeyAction -= OnKeyboard;
+// Managers.Input.KeyAction += OnKeyboard;
