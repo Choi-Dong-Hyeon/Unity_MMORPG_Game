52491c27fd846ba3aba9d65f6f0e527e18464926
ChoiDongHyeon <vwwwwvww@gmail.com>
2023-03-05 17:51:48 +0900

    commit

--- Assets/Script/UI/UI_Button.cs
+++ Assets/Script/UI/UI_Button.cs
@@ -6,13 +6,10 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UI_Button : MonoBehaviour
+public class UI_Button : UI_Base
 {
     [SerializeField]
     Text _text;
-    int score = 0;
-
-    Dictionary<Type, UnityEngine.Object[]> _objects = new Dictionary<Type, UnityEngine.Object[]>();
 
     enum Buttons
     {
@@ -27,66 +24,18 @@ public class UI_Button : MonoBehaviour
     {
         TestObject,
     }
-    
+
     private void Start()
     {
         Bind<Button>(typeof(Buttons));  //Buttons이름의 컴포넌트에 접근해서 <원하는컴포넌트>에 접근
         Bind<Text>(typeof(Texts));
         Bind<GameObject>(typeof(GameObjects));
 
-        Get<Text>((int)Texts.ScoreText).text = "Binding";
+        GetText((int)Texts.ScoreText).text = "Binding";
 
     }
 
-
-    void Bind<T>(Type type) where T : UnityEngine.Object
-    {
-       string[] names = Enum.GetNames(type);
-
-        UnityEngine.Object[] objects = new UnityEngine.Object[names.Length];
-        _objects.Add(typeof(T), objects);
-
-        for(int i=0; i < names.Length; i++)
-        {
-            if (typeof(T) == typeof(GameObject))
-            {
-
-                objects[i] = Util.FindChild(gameObject, names[i], true);
-            }
-            else
-            {
-                objects[i] = Util.FindChild<T>(gameObject, names[i], true);
-
-
-                if (objects[i] == null)
-                {
-                    Debug.Log($"바인드 실패{names[i]}");
-                }
-            }
-        }
-    }
-
-
-
-  
-
-    ///찾은걸 꺼내오는 작업
-    T Get<T>(int idx) where T : UnityEngine.Object
-    {
-        UnityEngine.Object[] objects = null;
-        if(_objects.TryGetValue(typeof(T),out objects)==false)
-        {
-            return null;
-        }
-        else
-        {
-            return objects[idx] as T;
-        }
-    }
-
-
-
-
+    int score = 0;
     public void OnButtonClick()
     {
 
